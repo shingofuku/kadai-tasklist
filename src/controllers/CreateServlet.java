bc15e45f3d7aa2e2a0550659b667bd8181fc93fd
shingo.fukuda <shfu6081@gmail.com>
2018-09-09 23:25:15 +0900

    Add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -18,7 +21,7 @@ import utils.DBUtil;
  */
 @WebServlet("/create")
 public class CreateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,34 +31,48 @@ public class CreateServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    String _token = (String)request.getParameter("_token");
-	    if(_token != null && _token.equals(request.getSession().getId())) {
-	        EntityManager em = DBUtil.createEntityManager();
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String _token = (String) request.getParameter("_token");
+        if (_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
 
-	        Task m = new Task();
+            Task m = new Task();
 
-	        String title = request.getParameter("title");
-	        m.setTitle(title);
+            String title = request.getParameter("title");
+            m.setTitle(title);
 
-	        String content = request.getParameter("content");
-	        m.setContent(content);
+            String content = request.getParameter("content");
+            m.setContent(content);
 
-	        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-	        m.setCreated_at(currentTime);
-	        m.setUpdated_at(currentTime);
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            m.setCreated_at(currentTime);
+            m.setUpdated_at(currentTime);
 
-	        em.getTransaction().begin();
-	        em.persist(m);
-	        em.getTransaction().commit();
-	        request.getSession().setAttribute("flush", "登録が完了しました。");
-	        em.close();
+            List<String> errors = TaskValidator.validate(m);
+            if (errors.size() > 0) {
+                em.close();
 
-	        response.sendRedirect(request.getContextPath() + "/index");
-		}
-	}
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+                rd.forward(request, response);
+            } else {
+
+                em.getTransaction().begin();
+                em.persist(m);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+                em.close();
+
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
+        }
+    }
 
 }
